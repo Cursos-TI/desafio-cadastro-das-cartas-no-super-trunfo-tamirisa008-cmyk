474b72c87e9b03a4f8704869af178f97b440c82e
tamirisa008-cmyk <tamirisa008@gmail.com>
2025-08-06 13:52:55 +0000

    criacao da variavel população

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,14 +9,18 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+    int populacao;
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
+    printf("Entre com o tamanho da populacao: ");
+    scanf("%d" , &populacao) ;
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
+    printf("A populacao tem: %d\n" , populacao);
+    
     return 0;
 }
